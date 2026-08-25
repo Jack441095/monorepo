c0dbf598c4ae134fc7b5b82d1378fa75ce103bb4
jackgandyae <jackgandyae@gmail.com>
2026-08-25 22:58:10 +0100

    fix(db): keep private-beta migration linear

--- backend/migrations/versions/6c3e9e1d4b7a_add_private_beta_release_channel.py
+++ backend/migrations/versions/6c3e9e1d4b7a_add_private_beta_release_channel.py
@@ -1,7 +1,7 @@
 """allow private-beta release channel
 
 Revision ID: 6c3e9e1d4b7a
-Revises: 90aab51af2f7
+Revises: 377a0020b4c5
 Create Date: 2026-08-25 23:00:00.000000
 
 """
@@ -11,7 +11,7 @@ from alembic import op
 
 
 revision: str = "6c3e9e1d4b7a"
-down_revision: Union[str, Sequence[str], None] = "90aab51af2f7"
+down_revision: Union[str, Sequence[str], None] = "377a0020b4c5"
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
 
